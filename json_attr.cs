ff3cae74ba3662f4f11cdf716c8fa2241eeb1a34
netnk <netnk0000@gmail.com>
2018-04-27 10:58:28 +0800

    Create json_attr.cs

---
+++ json_attr.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+public class Checkadmi
+{
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string userid { get; set; }
+    public string location { get; set; }
+}
+
+public class Login
+{
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public int field { get; set; }  
+    public float field { get; set; }  
+}
+
+public class Borrow
+{
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public string field { get; set; }  
+    public string field { get; set; }  
+}
